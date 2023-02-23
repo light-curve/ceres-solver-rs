9d6f57f99381e6161e8e16a5e5e959eea5f4cf42
Konstantin Malanchev <hombit@gmail.com>
2023-02-23 16:39:23 -0600

    sys: redo pkg-config stuff

--- ceres-solver-sys/build.rs
+++ ceres-solver-sys/build.rs
@@ -11,6 +11,8 @@ fn main() {
         cc_build.includes(std::env::split_paths(
             &std::env::var("DEP_CERES_INCLUDE").unwrap(),
         ));
+        println!("cargo:rustc-link-lib=static=glog");
+        println!("cargo:rustc-link-lib=static=ceres");
     }
     #[cfg(not(feature = "source"))]
     {
@@ -22,23 +24,15 @@ fn main() {
                 cc_build.include(path);
             });
         }
-    }
-    cc_build.compile("ceres-solver-sys");
-
-    #[cfg(feature = "source")]
-    {
-        println!("cargo:rustc-link-lib=static=glog");
-        println!("cargo:rustc-link-lib=static=ceres");
-    }
-    #[cfg(not(feature = "source"))]
-    {
-        if let Err(pkg_config_error) = pkg_config::Config::new()
-            // the earliest version tested, it may work with elder versions
+        match pkg_config::Config::new()
             .range_version("2.0.0".."3.0.0")
             .probe("ceres")
         {
-            dbg!(pkg_config_error);
-            println!("cargo:rustc-link-lib=ceres");
+            Ok(library) => library.include_paths.into_iter().for_each(|path| {
+                cc_build.include(path);
+            }),
+            Err(_) => println!("cargo:rustc-link-lib=ceres"),
         }
     }
+    cc_build.compile("ceres-solver-sys");
 }
